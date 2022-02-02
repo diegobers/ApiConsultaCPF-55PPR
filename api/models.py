825b803cf6329694b2958fadfd8679d20193f00f
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-02-01 22:54:59 -0300

    criação do modelo CommonMercosulNomenclature

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CommonMercosulNomenclature(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=5000)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    act_type = models.CharField(max_length=255)
+    act_number = models.CharField(max_length=255)
+    act_year = models.IntegerField()
+
+    def __str__(self):
+        return self.description
+
