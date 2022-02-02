39aab31ae359c419cb31dad5c920fa63f48747a5
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-02-01 22:57:15 -0300

    download/persistencia CommonMercosulNomenclature

--- api/views.py
+++ api/views.py
@@ -1,13 +1,22 @@
+from datetime import datetime
+
 from django.shortcuts import render
 import requests
 
+from api.models import CommonMercosulNomenclature
+
+
 def index(request):
-    #if request.method == 'POST':
-    downloadURL = requests.get('https://portalunico.siscomex.gov.br/classif/api/publico/nomenclatura/download/json')
-    
-    print(downloadURL.json())
-    print(downloadURL.status_code)
-    
-    #cpf = request.POST.get(['cpf'])
+    response = requests.get('https://portalunico.siscomex.gov.br/classif/api/publico/nomenclatura/download/json')
+    for item in response.json()['Nomenclaturas']:
+       CommonMercosulNomenclature.objects.get_or_create(
+            code=item['Codigo'],
+            description=item["Descricao"],
+            start_date=datetime.strptime(item["Data_Inicio"],'%d/%m/%Y'),
+            finish_date=datetime.strptime(item["Data_Fim"],'%d/%m/%Y'),
+            act_type=item["Tipo_Ato"],
+            act_number=item["Numero_Ato"],
+            act_year=item["Ano_Ato"],
+        )
 
     return render(request, 'index.html')
