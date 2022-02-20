0280df7eaf5f70a71a04dcb582ff4d75036865cd
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-02-20 18:35:29 -0300

    refactoring code

--- api/views.py
+++ api/views.py
@@ -9,11 +9,11 @@ from api.models import CommonMercosulNomenclature
 def index(request):
     response = requests.get('https://portalunico.siscomex.gov.br/classif/api/publico/nomenclatura/download/json')
     for item in response.json()['Nomenclaturas']:
-       CommonMercosulNomenclature.objects.get_or_create(
+        CommonMercosulNomenclature.objects.get_or_create(
             code=item['Codigo'],
             description=item["Descricao"],
-            start_date=datetime.strptime(item["Data_Inicio"],'%d/%m/%Y'),
-            finish_date=datetime.strptime(item["Data_Fim"],'%d/%m/%Y'),
+            start_date=datetime.strptime(item["Data_Inicio"], '%d/%m/%Y'),
+            finish_date=datetime.strptime(item["Data_Fim"], '%d/%m/%Y'),
             act_type=item["Tipo_Ato"],
             act_number=item["Numero_Ato"],
             act_year=item["Ano_Ato"],
